03885046e222ffac632555736d2281eef069bfda
Tasumaniadiabori <tasumaniadiabori@protonmail.com>
2020-07-08 23:56:15 +0300

    crud+Ref

--- src/controllers/messages.ts
+++ src/controllers/messages.ts
@@ -1,34 +1,37 @@
 import { Context } from 'koa'
-import { MessageModel } from '../models/messages'
+import { MessageModel, createMessage } from '../models/messages'
 import { Controller, Post, Get, Put, Delete } from 'koa-router-ts'
 
 @Controller('/messages')
 export default class {
-    @Post('/')
-    async postMessage(ctx: Context) {
-        await new MessageModel(ctx.request.body).save()
-        ctx.status=200
-    }
+  @Post('/:userId')
+  async postMessage(ctx: Context) {
+    await createMessage(ctx.params.userId, ctx.request.body)
+    ctx.status = 200
+  }
 
-    @Get('/')
-    async getMessages(ctx: Context) {
-        ctx.body = await MessageModel.find()
-    }
+  @Get('/')
+  async getMessages(ctx: Context) {
+    ctx.body = await MessageModel.find()
+  }
 
-    @Get('/:user')
-    async getMessagesFromUser(ctx: Context) {
-        ctx.body = await MessageModel.find({user: ctx.params.user})
-    }
+  @Get('/:userId')
+  async getMessagesFromUser(ctx: Context) {
+    ctx.body = await MessageModel.find({ userId: ctx.params.userId })
+  }
 
-    @Put('/:id')
-    async putMessage(ctx: Context) {
-        await MessageModel.findByIdAndUpdate({_id: ctx.params.id}, ctx.request.body)
-        ctx.status=200
-    }
+  @Put('/:id')
+  async putMessage(ctx: Context) {
+    await MessageModel.findOneAndUpdate(
+      { _id: ctx.params.id },
+      ctx.request.body
+    )
+    ctx.status = 200
+  }
 
-    @Delete('/:id')
-    async deleteMessage(ctx: Context) {
-        await MessageModel.findByIdAndDelete({_id: ctx.params.id})
-        ctx.status=200
-    }
+  @Delete('/:id')
+  async deleteMessage(ctx: Context) {
+    await MessageModel.findByIdAndDelete({ _id: ctx.params.id })
+    ctx.status = 200
+  }
 }
--- src/models/messages.ts
+++ src/models/messages.ts
@@ -1,11 +1,24 @@
-import { prop, getModelForClass, DocumentType } from '@typegoose/typegoose'
+import { prop, getModelForClass, Ref } from '@typegoose/typegoose'
+import { User } from '../models/user'
 
 export class Message {
-    @prop({ required: true, index: true })
-    user: string
+  @prop({ required: true })
+  userId: string
 
-    @prop({ required: true, index: true })
-    text: string
+  @prop({ required: true })
+  user: Ref<User>
+
+  @prop({ required: true })
+  text: string
 }
 
 export const MessageModel = getModelForClass(Message)
+
+export async function createMessage(userId, ctx) {
+  let message = await new MessageModel({
+    userId: userId,
+    user: ctx.user,
+    text: ctx.text,
+  }).save()
+  return message
+}
