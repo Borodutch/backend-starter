e920f44a59aeaecf50ab09305cb1c0e4fa6db377
Daniil Osmolovskiy <daniel.osmolovskiy@cience.com>
2020-07-08 15:58:41 +0300

    refactoring without commented prev code

--- src/app.ts
+++ src/app.ts
@@ -5,79 +5,13 @@ import * as Koa from 'koa'
 import * as json from 'koa-json'
 const mongoose = require('mongoose');
 import bodyParser from 'koa-bodyparser-ts'
-import { Context } from 'koa'
-import { loadControllers, Controller, Post, Get } from 'koa-router-ts'
+import { loadControllers } from 'koa-router-ts'
 import * as cors from '@koa/cors'
-import { MessageChannel } from 'worker_threads';
 
 const app = new Koa()
 const router = loadControllers(`${__dirname}/controllers`, { recurse: true })
 
 const Message = require('./controllers/message')
-
-//add a new message
-router.post('/addMsg', async ctx => {
-  try {
-    const result = await Message.addMsg({ ...ctx.request.body });
-    ctx.body = result;
-  } catch (err) {
-    ctx.status = 500;
-    ctx.body = 'Internal error'
-  }
-});
-
-//get message by ID
-router.get('/message/:id', async ctx => {
-  try {
-    const result = await Message.getMsg({ id: ctx.params.id });
-    ctx.body = result;
-  } catch (err) {
-    ctx.status = 500;
-    ctx.body = 'Internal error'
-  }
-})
-
-//update message by ID
-router.patch('/updateMsg', async ctx => {
-  try {
-    const result = await Message.updateMsg({ ...ctx.request.body });
-    ctx.body = result;
-  } catch (err) {
-    ctx.status = 500;
-    ctx.body = 'Internal error'
-  }
-})
-
-//delete message by ID
-// router.delete('/deleteMsg', async ctx => {
-//   try {
-//     const result = await Message.deleteMsg({ ...ctx.request.body });
-//     ctx.body = result;
-//   } catch (err) {
-//     ctx.status = 500;
-//     ctx.body = 'Internal error'
-//   }
-// })
-
-//get all messages
-// router.get('/', async ctx => {
-//   try {
-//     const result = await Message.getAllMsg({ id: ctx.params.id });
-//     ctx.body = result;
-//   } catch (err) {
-//     ctx.status = 500;
-//     ctx.body = 'Internal error'
-//   }
-// });
-
-// @Controller('/')
-// export default class {
-  // @Get('/')
-  // async getAll(ctx: Context) {
-    // const result = Message.getAllMsg({ id: ctx.params.id })
-    // ctx.body = result;
-//   }
-// }
 
 // Run app
 app.use(json());
--- src/models/message.ts
+++ src/models/message.ts
@@ -1,25 +1,6 @@
 import { prop, getModelForClass } from "@typegoose/typegoose";
-import { update } from "lodash";
 
 const mongoose = require('mongoose');
-
-// const Schema = new mongoose.Schema({
-//   date: {
-//     type: Date,
-//     default: new Date()
-//   },
-//   title: {
-//     type: String,
-//     required: true
-//   },
-//   text: {
-//     type: String,
-//     required: true,
-//     default: ''
-//   }
-// })
-
-// module.exports = mongoose.model('Message', Schema)
 
 export class MessageSchema {
   @prop({ required: false, default: new Date() })
@@ -31,8 +12,6 @@ export class MessageSchema {
   @prop({ required: true, default: '' })
   public text: String
 }
-
-const Message = getModelForClass(MessageSchema);
 
 export const MessageModel = getModelForClass(MessageSchema, {
   schemaOptions: { timestamps: true },
@@ -59,53 +38,3 @@ export async function createMessage({title, text}) {
   const message = MessageModel.create({title, text})
   return message
 }
-
-  // let message = MessageModel.create({title: 'Tst', text: 'hello, test!'})
-
-// import { sign } from '../helpers/jwt'
-// import { prop, getModelForClass, DocumentType } from '@typegoose/typegoose'
-// import { omit } from 'lodash'
-
-// export class Message {
-//   @prop({ index: true, default: new Date() })
-//   date: Date
-//   @prop({ index: true, lowercase: true, required: true })
-//   title: string
-//   @prop({ index: true, lowercase: true, default: '', required: true })
-//   text: string
-
-//   strippedAndFilled(withExtra = false, withToken = true) {
-//     const stripFields = ['createdAt', 'updatedAt', '__v']
-//     if (!withExtra) {
-//       stripFields.push('date')
-//       stripFields.push('title')
-//       stripFields.push('text')
-//     }
-//     return omit(this._doc, stripFields)
-//   }
-
-//   // Mongo property
-//   _doc: any
-// }
-
-// export const MessageModel = getModelForClass(Message, {
-//   schemaOptions: { timestamps: true },
-// })
-
-// interface LoginOptions {
-//   date: Date
-//   title: string
-//   text: string
-// }
-
-// export async function getOrCreateUser(loginOptions: LoginOptions) {
-//   if (!loginOptions.title) {
-//     throw new Error()
-//   }
-//   let message: DocumentType<Message> | undefined
-//   // Try text
-//   if (loginOptions.text) {
-//     message = await MessageModel.findOne({ email: loginOptions.text })
-//   }
-//   return message
-// }
