bac49c3dea846edd691e91dd20eecd333238e926
mormo669 <74607308+mormo669@users.noreply.github.com>
2021-01-26 17:43:18 +0300

    Unused code removed from "schema.ts"

--- src/models/schema.ts
+++ src/models/schema.ts
@@ -1,10 +1,10 @@
 import { ObjectId, ObjectID } from 'mongodb';
 import * as mongoose from 'mongoose'
 const Schema = mongoose.Schema;
-//save
+
 const messgSchema = new Schema(
     {
-        //_id: ObjectId,
+        
         title: String,
         body: String
     },
@@ -14,18 +14,3 @@ const messgSchema = new Schema(
 )
 const messgModel = mongoose.model('message', messgSchema)
 export default messgModel
-
-//read
-
-// const readSchema = new Schema(
-//     {
-//         _id: ObjectId,
-//         title: String,
-//         message: String
-//     },
-//     {
-//         timestamps: true
-//     }
-// )
-// export const readModel = mongoose.model('?', readSchema)
- 
