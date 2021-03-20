d4a4bdfc895b582d04f6857511d5e7c440f32fda
mormo669 <74607308+mormo669@users.noreply.github.com>
2021-03-20 15:05:46 +0300

    CRUD added with message.ts and schema.ts

---
+++ src/controllers/message.ts
@@ -0,0 +1,58 @@
+import { Controller, Get, Post, Delete, Put, Ctx } from 'koa-ts-controllers'
+import { Context } from 'koa'
+import { getOrCreateMessage, MessageModel } from '../models/schema'
+
+@Controller('/message')
+export default class MessageController {
+  @Post('/save')
+  async addMessage(@Ctx() ctx: Context) {
+    let msg = getOrCreateMessage(ctx.request.body)
+    ctx.body = { msg }
+  }
+  @Get('/read/:id')
+  async getMessages(@Ctx() ctx: Context) {
+    let msg = await MessageModel.findById(
+      {
+        _id: ctx.params.id,
+      },
+      (error, data) => {
+        if (error) {
+          console.log(error)
+        } else {
+          console.log('data:', data)
+        }
+      }
+    )
+
+    ctx.body = { msg }
+  }
+  @Delete('/del/:id')
+  async deleteMessage(@Ctx() ctx: Context) {
+    await MessageModel.findByIdAndDelete({
+      _id: ctx.params.id,
+    })
+  }
+
+  @Put('/upd/:id')
+  async updateMessage(@Ctx() ctx: Context) {
+    ctx.body = await MessageModel.findByIdAndUpdate(
+      {
+        _id: ctx.params.id,
+      },
+      {
+        $set: {
+          title: ctx.request.body.title,
+          body: ctx.request.body.body,
+        },
+        timestamps: true,
+      },
+      (error) => {
+        if (error) {
+          console.log(error)
+        }
+      }
+    ).then((data) => {
+      return data
+    })
+  }
+}
---
+++ src/models/schema.ts
@@ -0,0 +1,36 @@
+import { prop, getModelForClass, DocumentType } from '@typegoose/typegoose'
+
+export class Message {
+  @prop({ index: true, lowercase: true })
+  title: String
+  @prop({ index: true, lowercase: true })
+  body: String
+}
+
+export const MessageModel = getModelForClass(Message, {
+  schemaOptions: { timestamps: true },
+})
+
+export async function getOrCreateMessage(msg: Message) {
+  if (!msg.title && !msg.body) {
+    throw new Error()
+  }
+
+  let message: DocumentType<Message> | undefined
+  const params = {
+    title: msg.title,
+    body: msg.body,
+  } as any
+  if (msg.title) {
+    params.title = msg.title
+  }
+  if (msg.body) {
+    params.body = msg.body
+  }
+
+  message = await new MessageModel({
+    ...params,
+  }).save()
+
+  return message
+}
