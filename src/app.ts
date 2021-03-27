da60db570a6105d980969281499c9bfba2b2f447
borshyo <63948052+borshyo@users.noreply.github.com>
2021-03-27 17:03:17 +0300

    test

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,11 @@ import { bootstrapControllers } from 'koa-ts-controllers'
 import * as cors from '@koa/cors'
 import { runMongo } from '@/models/index'
 import * as Router from 'koa-router'
+import * as render from 'koa-ejs'
+import * as serve from 'koa-static'
+import * as path from 'path'
+import {getIndex, getAdd, add, deleteMessage, updateMessage} from './controllers/crudController.js'
+import { facebookLogin, telegramLogin, checkUser, logout} from './controllers/authController.js'
 
 export const app = new Koa()
 ;(async () => {
@@ -16,12 +21,33 @@ export const app = new Koa()
       controllers: [__dirname + '/controllers/*'],
       disableVersioning: true,
     })
+    
     app.use(cors({ origin: '*' }))
-    app.use(cors({ origin: '*' }))
+    app.use(cors({ origin: '*' }))  
     app.use(bodyParser())
     app.use(router.routes())
     app.use(router.allowedMethods())
+    app.use(serve('./public'))
+    
+    render(app, {
+      root: path.join(__dirname, 'views'),
+      layout: 'layout',
+      viewExt: 'html',
+      cache: false,
+      debug: false,
+  })
+    router.use(checkUser)
+    router.get('/', getIndex)
+    router.get('/add', getAdd)
+    router.post('/add', add)
+    router.delete('/', deleteMessage)
+    router.put('/', updateMessage)
+    router.post('/facebooklogin', facebookLogin)
+    router.post('/telegramlogin', telegramLogin)
+    router.get('/logout', logout)
+  
   } catch (err) {
     console.log('Koa app starting error: ', err)
   }
 })()
+
