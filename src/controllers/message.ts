0fdda96832d9c9a27f05d2dc46d194b5cfb37e48
borshyo <63948052+borshyo@users.noreply.github.com>
2021-03-30 10:02:48 +0300

    async await fixes, prettier fix

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -5,36 +5,63 @@ import { Controller, Ctx, Get, Post, Put, Delete } from 'koa-ts-controllers'
 
 @Controller('/messages')
 export default class MessageController {
-    @Get('/')
-    async getAllMessages(@Ctx() ctx: Context) {
-        await MessageModel.find().sort({createdAt: -1})
-        .then(result =>ctx.res.end(JSON.stringify(result)))
-        .catch(err =>console.log(err))
+  @Get('/')
+  async getAllMessages(@Ctx() ctx: Context) {
+    try {
+      let result = await MessageModel.find().sort({ createdAt: -1 })
+      ctx.res.end(JSON.stringify(result))
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    @Get('/:id')
-    async getOneMessage(@Ctx() ctx: Context) {
-        await MessageModel.findById(ctx.params.id)
-        .then(result =>ctx.res.end(JSON.stringify(result)))
-        .catch(err => console.log(err))
+  @Get('/:id')
+  async getOneMessage(@Ctx() ctx: Context) {
+    try {
+      let result = await MessageModel.findById(ctx.params.id)
+      ctx.res.end(JSON.stringify(result))
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    @Post('/add')
-    async postMessage(@Ctx() ctx: Context) {
-        const user = await getOrCreateUser({telegramId: ctx.request.body.telegramId, facebookId: ctx.request.body.facebookId, email: ctx.request.body.email, name: ctx.request.body.name})
-        await MessageModel.create({author: user, title: ctx.request.body.title, body: ctx.request.body.body})
-        .catch(err => console.log(err))
+  @Post('/add')
+  async postMessage(@Ctx() ctx: Context) {
+    try {
+      const user = await getOrCreateUser({
+        telegramId: ctx.request.body.telegramId,
+        facebookId: ctx.request.body.facebookId,
+        email: ctx.request.body.email,
+        name: ctx.request.body.name,
+      })
+      const message = await new MessageModel({
+        author: user,
+        title: ctx.request.body.title,
+        body: ctx.request.body.body,
+      }).save()
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    @Put('/:id')
-    async putMessage(@Ctx() ctx: Context) {
-        await MessageModel.findByIdAndUpdate(ctx.params.id, {title: ctx.request.body.title, body: ctx.request.body.body})
-        .catch(err => console.log(err))
+  @Put('/:id')
+  async putMessage(@Ctx() ctx: Context) {
+    try {
+      await MessageModel.findByIdAndUpdate(ctx.params.id, {
+        title: ctx.request.body.title,
+        body: ctx.request.body.body,
+      })
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    @Delete('/:id')
-    async deleteMessage(@Ctx() ctx: Context) {
-        await MessageModel.findByIdAndDelete(ctx.params.id)
-        .catch(err => console.log(err))
+  @Delete('/:id')
+  async deleteMessage(@Ctx() ctx: Context) {
+    try {
+      await MessageModel.findByIdAndDelete(ctx.params.id)
+    } catch (err) {
+      console.log(err)
     }
+  }
 }
--- src/models/message.ts
+++ src/models/message.ts
@@ -1,15 +1,15 @@
-import { prop, getModelForClass } from '@typegoose/typegoose';
-import { User } from '@/models/user';
+import { prop, getModelForClass } from '@typegoose/typegoose'
+import { User } from '@/models/user'
 
 export class Message {
-    @prop({index: true, required: true})
-    title: string
-    @prop({index: true, required: true})
-    body: string
-    @prop({index: true, required: true})
-    author: User
+  @prop({ index: true, required: true })
+  title: string
+  @prop({ index: true, required: true })
+  body: string
+  @prop({ index: true, required: true, unique: false })
+  author: User
 }
 
 export const MessageModel = getModelForClass(Message, {
-    schemaOptions: { timestamps: true },
+  schemaOptions: { timestamps: true },
 })
