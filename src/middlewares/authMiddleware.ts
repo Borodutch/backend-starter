f4c13129b67ee5a6924fb4242ac6c4d532095611
Ковтун Аркадий <frozo13@gmail.com>
2021-04-30 19:37:16 +0300

    перевел на try/catch

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,17 +3,16 @@ import { verify } from '@/helpers/jwt'
 import { getOrCreateUser } from '@/models/user'
 
 export async function authMiddleware(ctx: Context, next) {
-  await verify(ctx.headers.token as string)
-    .then(async payload => {
-      const user = await getOrCreateUser({
-        name: payload['email'],
-        email: payload['email'],
-      })
-      ctx.request.body['userId'] = user['_id']
-      return next()
+  try {
+    const payload = await verify(ctx.headers.token as string)
+    const user = await getOrCreateUser({
+      name: payload['email'],
+      email: payload['email'],
     })
-    .catch(error => {
-      console.log(error)
-      return ctx.throw(401)
-    })
+    ctx.request.body['userId'] = user['_id']
+    return next()
+  } catch (error) {
+    console.log(error)
+    return ctx.throw(401)
+  }
 }
