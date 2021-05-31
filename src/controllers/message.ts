9a4e32bf565bcde5bbc9b4fa3e785c2372ffe6af
Programistich <alexasda051@gmail.com>
2021-05-31 22:13:44 +0300

    refactor

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -1,41 +1,39 @@
 import { Context } from 'koa'
 import { Controller, Ctx, Delete, Get, Post, Put, Flow } from 'amala'
 import {
-    getMessages,
-    getMessageById,
-    addMessage,
-    updateMessageById,
-    deleteMessageById,
+  getMessages,
+  getMessageById,
+  addMessage,
+  updateMessageById,
+  deleteMessageById,
 } from '@/models/msg'
 import { auth } from '@/middleware/auth'
 
 @Flow(auth)
 @Controller('/messages')
 export default class MessageController {
-    @Get('/')
-    async getMessages(@Ctx() ctx: Context) {
-        return await getMessages(ctx.state.userID)
-    }
+  @Get('/')
+  async getMessages(@Ctx() ctx: Context) {
+    return await getMessages(ctx.state.user._id)
+  }
 
-    @Get('/:id')
-    async getMessage(@Ctx() ctx: Context) {
-        let id = ctx.params.id
-        return await getMessageById(ctx.state.userID, id)
-    }
+  @Get('/:id')
+  async getMessage(@Ctx() ctx: Context) {
+    return await getMessageById(ctx.state.user._id, ctx.params.id)
+  }
 
-    @Post('/')
-    async createMessage(@Ctx() ctx: Context) {
-        return addMessage(ctx.state.userID, ctx.state.text)
-    }
+  @Post('/')
+  async createMessage(@Ctx() ctx: Context) {
+    return addMessage(ctx.state.user._id, ctx.state.text)
+  }
 
-    @Put('/')
-    async updateMessage(@Ctx() ctx: Context) {
-        return updateMessageById(ctx.state.userID, ctx.state.id, ctx.state.text)
-    }
+  @Put('/')
+  async updateMessage(@Ctx() ctx: Context) {
+    return updateMessageById(ctx.state.user._id, ctx.state.id, ctx.state.text)
+  }
 
-    @Delete('/:id')
-    async deleteMessage(@Ctx() ctx: Context) {
-        let id = ctx.params.id
-        return await deleteMessageById(ctx.state.userID, id)
-    }
+  @Delete('/:id')
+  async deleteMessage(@Ctx() ctx: Context) {
+    return await deleteMessageById(ctx.state.user._id, ctx.params.id)
+  }
 }
--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,16 +3,14 @@ import { verify } from '@/helpers/jwt'
 import { getOrCreateUser } from '@/models/user'
 
 export async function auth(ctx: Context, next) {
-    try {
-        const payload = await verify(ctx.headers.token as string)
-        const user = await getOrCreateUser({
-            name: payload['name'],
-            email: payload['email'],
-        })
-        ctx.state = user
-        ctx.request.body['userID'] = user['_id']
-        return next()
-    } catch (e) {
-        return ctx.throw(401)
-    }
+  try {
+    const payload = await verify(ctx.headers.token as string)
+    ctx.state.user = await getOrCreateUser({
+      name: payload['name'],
+      email: payload['email'],
+    })
+    return next()
+  } catch (e) {
+    return ctx.throw(401)
+  }
 }
