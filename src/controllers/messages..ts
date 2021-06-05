b91fd46b13cd52e4ba294ae26c0ae9a9030ff3d5
overstar <8497715@gmail.com>
2021-06-06 01:32:37 +0500

    Dummy CRUD for messages

--- src/controllers/messages..ts
+++ src/controllers/messages..ts
@@ -1,15 +1,57 @@
-import { Context } from 'koa'
-import { Controller, Ctx, Post, Get } from 'amala'
+import { Context } from 'koa';
+import { Controller, Ctx, Post, Get, Params, Delete, Body } from 'amala';
 
-@Controller('/')
-export default class MessageController {
-  @Get('/msg')
-  async myEndpointHandler() {
-      // GET /api/v1/foo OR /api/v2/foo OR /api/vdangote/foo
-
-      return {myMessage: 'hello one!'};
-  }
+interface backResponce {
+  message?: string;
+  id?: number;
+  body?: object;
 }
 
+@Controller('/api')
+export default class MessageController {
+  
+  @Get('/message')
+  async getMessages() {
+    const result: backResponce = {
+      message: 'Show all messages'
+    };
+    return result;
+  }
 
+  @Post('/message')
+  async createMessage(@Ctx() ctx: Context) {
+    const result: backResponce = {
+      message: 'Create a new message',
+      body: ctx.request.body,
+    };
+    return result;
+  }
 
+  @Get('/message/:id')
+  async getMessageById(@Params('id') id: number) {
+    const result: backResponce = {
+      id: id,
+      message: 'Show message with id',
+    };
+    return result;
+  }
+
+  @Post('/message/:id')
+  async updateMessageById(@Ctx() ctx: Context, @Params('id') id: number) {
+    const result: backResponce = {
+      id: id,
+      message: 'Update message with id',
+      body: ctx.request.body,
+    };
+    return result;
+  }
+
+  @Delete('/message/:id')
+  async deleteMessage(@Params('id') id: number) {
+    const result: backResponce = {
+      id: id,
+      message: 'Delete message with id',
+    };
+    return result;
+  }
+}
