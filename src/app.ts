6fb8cdffa2cba430db5a20d937f2bb2e4a81d63f
gluknik <gluknik@gmail.com>
2021-06-08 22:58:42 +0300

    Returns some files to their starting point

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import 'reflect-metadata'
 import * as Koa from 'koa'
 import * as bodyParser from 'koa-bodyparser'
-import { bootstrapControllers, Ctx, getControllers } from 'koa-ts-controllers'
+import { bootstrapControllers} from 'koa-ts-controllers'
 import * as cors from '@koa/cors'
 import { runMongo } from '@/models/index'
 import * as Router from 'koa-router'
-import MessageController from '@/controllers/message'
 
 export const app = new Koa()
 ;(async () => {
@@ -22,28 +21,6 @@ export const app = new Koa()
     app.use(bodyParser())
     app.use(router.routes())
     app.use(router.allowedMethods())
-
- 
-    const messageHandler = new MessageController();
-    // Create a new message
-    // Example http://127.0.0.1:1337/message?body=test&createdBy=Nick
-    router.post('/message', messageHandler.create)
-
-    // Get a message
-    // Example http://127.0.0.1:1337/message/60bb41a4d4a24632a81a697b
-    router.get('/message/:id', messageHandler.getOne)
-
-    // Get all messages
-    router.get('/messages', messageHandler.getAll)
-
-    // Update a message
-    // Example http://127.0.0.1:1337/message/60bb3e3cb0c4dc2fa17a8b65?body=message
-    router.patch('/message/:id', messageHandler.update)
-
-    // Delete a message
-    // Example: http://127.0.0.1:1337/message/60bb568c9ca14e127590375c
-    router.delete('/message/:id', messageHandler.delete)
-
   } catch (err) {
     console.log('Koa app starting error: ', err)
   }
