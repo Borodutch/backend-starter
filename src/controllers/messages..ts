ecd545a0fbd674b276167ab0a86c5523cf0e078d
overstar <8497715@gmail.com>
2021-06-10 00:15:27 +0500

    some minor restore

--- src/controllers/messages..ts
+++ src/controllers/messages..ts
@@ -1,48 +1,47 @@
-import { Context } from 'koa';
-import { Controller, Ctx, Post, Get, Params, Delete, Body } from 'amala';
-import { Message, MessageModel, } from '@/models/message';
-import { DocumentType } from '@typegoose/typegoose';
+import { Context } from 'koa'
+import { Controller, Ctx, Post, Get, Params, Delete } from 'amala'
+import { Message, MessageModel } from '@/models/message'
+import { DocumentType } from '@typegoose/typegoose'
 
 const CURREN_USER_NAME = 'user'
 
 @Controller('/api')
 export default class MessageController {
-  
   @Get('/message')
   async getMessages() {
-    const filter = {name : CURREN_USER_NAME}
-    const msg: DocumentType<Message>[] = await MessageModel.find(filter)
-    return msg;
+    const filter = { name: CURREN_USER_NAME }
+    const msg = await MessageModel.find(filter)
+    return msg
   }
 
   @Post('/message')
   async createMessage(@Ctx() ctx: Context) {
     const doc = ctx.request.body.doc
-    const msg: DocumentType<Message> = await MessageModel.create(doc)
+    const msg = await MessageModel.create(doc)
 
-    return msg;
+    return msg
   }
 
   @Get('/message/:id')
   async getMessageById(@Params('id') id: string) {
-    const filter = {_id : id, name : CURREN_USER_NAME}
-    const msg: DocumentType<Message> = await MessageModel.findOne(filter)
-    return msg;
+    const filter = { _id: id, name: CURREN_USER_NAME }
+    const msg = await MessageModel.findOne(filter)
+    return msg
   }
 
   @Post('/message/:id')
   async updateMessageById(@Ctx() ctx: Context, @Params('id') id: string) {
-    const filter = {_id : id, name : CURREN_USER_NAME}
+    const filter = { _id: id, name: CURREN_USER_NAME }
     const doc = ctx.request.body.doc
-    let msg: DocumentType<Message> = await MessageModel.findOneAndUpdate(filter, doc)
-    msg  = await MessageModel.findOne(filter)
-    return msg;
+    let msg = await MessageModel.findOneAndUpdate(filter, doc)
+    msg = await MessageModel.findOne(filter)
+    return msg
   }
 
   @Delete('/message/:id')
   async deleteMessage(@Params('id') id: string) {
-    const filter = {_id : id, name : CURREN_USER_NAME}
-    const msg: DocumentType<Message> = await MessageModel.findOneAndDelete(filter)
-    return msg;
+    const filter = { _id: id, name: CURREN_USER_NAME }
+    const msg = await MessageModel.findOneAndDelete(filter)
+    return msg
   }
 }
--- src/models/message.ts
+++ src/models/message.ts
@@ -1,18 +1,13 @@
 import { prop, getModelForClass, DocumentType } from '@typegoose/typegoose'
 
 export class Message {
-
-  @prop({required: true, index: true})
+  @prop({ required: true, index: true })
   message: string
-  
-  @prop({ required: true, index: true, unique: true  })
-  name: string
 
-  
-  // Mongo property
-  //_doc: any
+  @prop({ required: true, index: true, unique: true })
+  name: string
 }
 
 export const MessageModel = getModelForClass(Message, {
-  schemaOptions: { timestamps: true, collection: 'messages'}
+  schemaOptions: { timestamps: true, collection: 'messages' },
 })
