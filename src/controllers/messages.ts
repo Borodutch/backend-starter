a7b4881fc24f3c408809d9efa063a78db48810d6
Earth Spacon <rahmatjulmat@gmail.com>
2021-06-19 21:40:52 +0500

    installed prettier

--- src/controllers/messages.ts
+++ src/controllers/messages.ts
@@ -1,27 +1,29 @@
-import { MsgsModel } from '@/models/message'
-import { Controller, Ctx, Get, Post, Put, Delete } from 'amala'
+import { MsgsModel } from "@/models/message";
+import { Controller, Ctx, Get, Post, Put, Delete } from "amala";
 
-@Controller('/message')
+@Controller("/message")
 export default class {
-    @Post('/create')
-    async create(@Ctx() ctx) {
-        const newMsg = new MsgsModel(ctx.request.body)
-        await newMsg.save()
-    }
+  @Post("/create")
+  async create(@Ctx() ctx) {
+    const newMsg = new MsgsModel(ctx.request.body);
+    await newMsg.save();
+  }
 
-    @Get('/read/:id')
-    async read(@Ctx() ctx) {
-        const someMsg = await MsgsModel.findById(ctx.params.id)
-        return someMsg
-    }
+  @Get("/read/:id")
+  async read(@Ctx() ctx) {
+    const someMsg = await MsgsModel.findById(ctx.params.id);
+    return someMsg;
+  }
 
-    @Put('/update/:id')
-    async update(@Ctx() ctx) {
-        await MsgsModel.findByIdAndUpdate(ctx.params.id, { message: ctx.request.body })
-    }
+  @Put("/update/:id")
+  async update(@Ctx() ctx) {
+    await MsgsModel.findByIdAndUpdate(ctx.params.id, {
+      message: ctx.request.body,
+    });
+  }
 
-    @Delete('/delete/:id')
-    async delete(@Ctx() ctx) {
-       await MsgsModel.findByIdAndDelete(ctx.params.id)
-    }
+  @Delete("/delete/:id")
+  async delete(@Ctx() ctx) {
+    await MsgsModel.findByIdAndDelete(ctx.params.id);
+  }
 }
--- src/models/message.ts
+++ src/models/message.ts
@@ -1,8 +1,9 @@
-import { prop, getModelForClass } from '@typegoose/typegoose'
+import { prop, getModelForClass } from "@typegoose/typegoose";
 
 class Messages {
-    @prop({ required: true, index: true })     
-    messages?: string
-}   
-export const MsgsModel = getModelForClass(Messages, 
-    { schemaOptions: { timestamps: true }, })
+  @prop({ required: true, index: true })
+  messages?: string;
+}
+export const MsgsModel = getModelForClass(Messages, {
+  schemaOptions: { timestamps: true },
+});
