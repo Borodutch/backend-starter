ba64c9ae1eb8eeee17f989aa200f2fa2e5f2caa4
kaa777 <56987310+kaa777@users.noreply.github.com>
2021-06-21 18:33:32 -0400

    Commented code / comments removed

--- src/models/messageModel.ts
+++ src/models/messageModel.ts
@@ -2,19 +2,16 @@ import { getModelForClass, prop } from "@typegoose/typegoose";
 import { User } from "./user";
 
 export class Message {
-    //TODO: Add user authentication
-    // @prop({ required: true, index: true })
-    // user: User  
     @prop({ required: true, index: true })
     body: string
 }
 
 export const MessageModel = getModelForClass(Message, {
     schemaOptions: { timestamps: true },
-  })
+})
 
 export async function createMessage(body) {
-const message = new MessageModel({body: body}).save()
+    const message = new MessageModel({ body: body }).save()
 }
 
 export async function showAllMessages() {
@@ -28,9 +25,9 @@ export async function findMessageById(id: string) {
 }
 
 export async function deleteMessageById(id: string) {
-    await MessageModel.deleteOne({_id: id});
+    await MessageModel.deleteOne({ _id: id });
 }
 
 export async function updateMessage(id: string, body: string) {
-    await MessageModel.updateOne({_id: id}, {body: body})
+    await MessageModel.updateOne({ _id: id }, { body: body });
 }
