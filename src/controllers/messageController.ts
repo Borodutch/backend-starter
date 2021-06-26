558363cce5898dc314b44af5d2233bd89ea4c7ba
kaa777 <56987310+kaa777@users.noreply.github.com>
2021-06-26 17:35:04 -0400

    refactored properly using amala’s @Body & @Params

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -1,38 +1,72 @@
 import {
   createMessage,
-  showAllMessages,
+  getMessages,
   findMessageById,
   deleteMessageById,
   updateMessage,
-} from '@/models/messageModel'
-import { Controller, Ctx, Get, Post, Delete, Flow, Params, Put } from 'amala'
+} from '@/models/message'
+import {
+  Controller,
+  Ctx,
+  Get,
+  Post,
+  Delete,
+  Flow,
+  Params,
+  Put,
+  Body,
+} from 'amala'
 import { Context } from 'koa'
 
 @Controller('/messages')
 class messageController {
   @Get('/')
   async messageList(@Ctx() ctx: Context) {
-    return showAllMessages()
+    try {
+      return await getMessages()
+    } catch (error) {
+      console.log(error)
+    }
   }
   @Post('/add')
-  async messageAdd(@Ctx() ctx: Context) {
-    const data: string = ctx.request.body
-    createMessage(data)
+  async messageAdd(@Body() addData: string) {
+    try {
+      createMessage(addData)
+    } catch (error) {
+      console.log(error)
+    }
   }
   @Get('/:id')
-  async showSingleMessage(@Ctx() ctx: Context, @Params('id') id: string) {
-    const messageId = ctx.params.id
-    return findMessageById(messageId)
+  async showSingleMessage(
+    @Ctx() ctx: Context,
+    @Params('id') messageId: string
+  ) {
+    try {
+      return findMessageById(messageId)
+    } catch (error) {
+      console.log(error)
+    }
   }
   @Delete('/:id')
-  async deleteSingleMessage(@Ctx() ctx: Context, @Params('id') id: string) {
-    const messageId = ctx.params.id
-    deleteMessageById(messageId)
+  async deleteSingleMessage(
+    @Ctx() ctx: Context,
+    @Params('id') messageId: string
+  ) {
+    try {
+      await deleteMessageById(messageId)
+    } catch (error) {
+      console.log(error)
+    }
   }
   @Put('/:id')
-  async updateSingleMessage(@Ctx() ctx: Context, @Params('id') id: any) {
-    const messageId = ctx.params.id
-    const data = ctx.request.body
-    updateMessage(messageId, data)
+  async updateSingleMessage(
+    @Params('id') messageId: any,
+    @Body() putData: string
+  ) {
+    try {
+      await updateMessage(messageId, putData)
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
