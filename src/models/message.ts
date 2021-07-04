0bed15fd57b0f828949ffa2005187fd3f388b397
Daniel <dpancov64@gmail.com>
2021-07-04 14:52:37 +0300

    Added async/await for certain functions

--- src/models/message.ts
+++ src/models/message.ts
@@ -17,18 +17,18 @@ export async function createMessage(user: User, text: Message) {
   return await new MessageModel({ user, text }).save()
 }
 
-export function readMessageById(id: string) {
-  return MessageModel.findById(id)
+export async function readMessageById(id: string) {
+  return await MessageModel.findById(id)
 }
 
-export function readMessagesByUser(user: User) {
-  return MessageModel.find(user)
+export async function readMessagesByUser(user: User) {
+  return await MessageModel.find(user)
 }
 
-export function updateMessageById(id: string, user: User, text: object) {
-  return MessageModel.findOneAndUpdate({ id, user }, text)
+export async function updateMessageById(id: string, user: User, text: object) {
+  return await MessageModel.findOneAndUpdate({ id, user }, text)
 }
 
-export function deleteMessageById(user: User, id: string) {
-  return MessageModel.findOneAndDelete({ _id: id, user })
+export async function deleteMessageById(user: User, id: string) {
+  return await MessageModel.findOneAndDelete({ _id: id, user })
 }
