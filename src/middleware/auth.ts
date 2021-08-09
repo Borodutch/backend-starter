a7216d89467e79ab202856a89b3382c9f397695d
Stepan Sergeyev <sergeev404@gmail.com>
2021-08-09 12:41:43 -0400

    added middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,21 @@
+import { MessageModel } from '@/models/message'
+import { UserModel } from '@/models/user'
+import { Context } from 'koa'
+
+export async function authMiddleware(ctx: Context, next) {
+  const token = ctx.headers.token
+  const messageId = ctx.params.id
+  if (typeof token !== 'string') {
+    return ctx.throw(401, 'Invalid token')
+  }
+  const user = await UserModel.findOne({ token: token })
+  const message = await MessageModel.findById(messageId)
+  if (!token) {
+    return ctx.throw(401, "You're not logged in")
+  }
+
+  if (user.token !== message.author.token) {
+    return ctx.throw(401, 'invalid auth')
+  }
+  await next()
+}
