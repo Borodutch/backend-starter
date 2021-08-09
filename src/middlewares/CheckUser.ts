9afd040d191ceeb3fbcdb6a103e13e061e79feb2
Jordan Mikel <nursik551@gmail.com>
2021-08-09 21:13:41 +0600

    Middleware

---
+++ src/middlewares/CheckUser.ts
@@ -0,0 +1,9 @@
+import { Context, Next } from 'koa'
+import { MessageModel } from '@/models/message'
+export async function CheckUser(ctx: Context, next: Next) {
+  const message = await MessageModel.findById(ctx.params.id)
+  const user = ctx.state.user
+  if (message.user == user.id) {
+    return next()
+  }
+}
