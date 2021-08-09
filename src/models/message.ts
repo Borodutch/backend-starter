10b67fd6878266b3eff559f3f127ceb215333e36
Jordan Mikel <nursik551@gmail.com>
2021-08-09 21:06:26 +0600

    cleaned code

--- src/models/message.ts
+++ src/models/message.ts
@@ -12,22 +12,3 @@ export class Message {
 export const MessageModel = getModelForClass(Message, {
   schemaOptions: { timestamps: true },
 })
-
-export function createMessage(user: User, text: string) {
-  return new MessageModel({
-    user,
-    text,
-  }).save()
-}
-
-export function readMessages(user: User, text: string) {
-  return MessageModel.find({user, text})
-}
-
-export function updateMessageById(_user: User, id: string, text: string) {
-  return MessageModel.findByIdAndUpdate({ id, text })
-}
-
-export function deleteMessageById(user: User, id: string) {
-  return MessageModel.findByIdAndDelete({ _id: id, user })
-}
