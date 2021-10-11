f0158764a39c482c60e92a2075081b83878a4bc4
backmeupplz <backmeupplz@gmail.com>
2021-10-11 10:58:55 -0700

    upgrade dependencies

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,12 +1,7 @@
 import * as mongoose from 'mongoose'
 
 export function runMongo(mongoUrl = process.env.MONGO) {
-  return mongoose.connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: true,
-  })
+  return mongoose.connect(mongoUrl)
 }
 export function stopMongo() {
   return mongoose.disconnect()
