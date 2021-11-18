275e64a5f5a5ee705a44b56a7f5a6750183835fc
Alexander <aleks.rahimov@gmail.com>
2021-11-18 22:02:51 +0300

    Fixed linter

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,7 @@
 import { Context, Next } from 'koa'
 import { UserModel } from '@/models/user'
 
-export const userAuth = async (ctx: Context, next: Next) => {
+export default async function userAuth(ctx: Context, next: Next) {
   const user = await UserModel.findOne({ filter: ctx.headers.token })
   if (!user) {
     return ctx.throw(404, 'User not found')
