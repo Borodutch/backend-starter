25c497402d2435010e1f5cc3d41f0c1b5930c553
Alexander <aleks.rahimov@gmail.com>
2021-11-19 18:44:17 +0300

    Added validation and verification of token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,11 +1,18 @@
 import { Context, Next } from 'koa'
 import { UserModel } from '@/models/user'
+import { verify } from '@/helpers/jwt'
 
 export default async function userAuth(ctx: Context, next: Next) {
+  const verification = await verify(ctx.headers.token as string)
+  if (!verification) {
+    return ctx.throw(404, 'Token is not valid')
+  }
+
   const user = await UserModel.findOne({ filter: ctx.headers.token })
   if (!user) {
     return ctx.throw(404, 'User not found')
   }
+
   ctx.state.user = user
 
   return next()
