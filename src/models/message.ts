b4e71a85f11c75ad4b8a3b27763d037ac121549d
Bleb1k <viktor.masnikov17283940@gmail.com>
2021-11-29 09:46:58 +0900

    no ", Types.ObjectId"

--- src/models/message.ts
+++ src/models/message.ts
@@ -1,12 +1,11 @@
 import { Ref, getModelForClass, prop } from '@typegoose/typegoose'
-import { Types } from 'mongoose'
 import { User } from '@/models/user'
 
 export class Message {
   @prop({ required: true })
   text!: string
   @prop({ required: true, index: true, ref: () => User })
-  author!: Ref<User, Types.ObjectId>
+  author!: Ref<User>
 }
 
 export const MessageModel = getModelForClass(Message)
