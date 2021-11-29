5e73b2fc0913e57596c15358f17b0163f7f16405
Nikita Kolmogorov <backmeupplz@gmail.com>
2021-11-29 14:19:54 -0800

    add node/no-process-env

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,5 +1,6 @@
 import { cleanEnv, num, str } from 'envalid'
 
+// eslint-disable-next-line node/no-process-env
 export default cleanEnv(process.env, {
   PORT: num({ default: 1337 }),
   FACEBOOK_APP_ID: str(),
