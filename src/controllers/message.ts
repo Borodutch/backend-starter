c6af9a5feb6080cad8c90b6a71420c2a10308cce
pchchv <ipchchv@gmail.com>
2022-01-26 19:20:07 +0300

    Add all user messages from the db

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Post } from 'amala'
-import { createMessage } from '@/models/Message'
+import { Body, Controller, Get, Header, Post } from 'amala'
+import { createMessage, getMessages } from '@/models/Message'
 
 @Controller('/message')
 export default class MessageController {
   @Post('/')
-  async addMessage(@Body('user') user: string, @Body('text') text: string) {
+  async addMessage(@Header('user') user: string, @Header('text') text: string) {
     return await createMessage(user, text)
+  }
+  @Get('/')
+  async getMessages(@Header('user') user: string) {
+    return await getMessages(user)
   }
 }
--- src/models/Message.ts
+++ src/models/Message.ts
@@ -1,14 +1,26 @@
-import { getModelForClass, prop } from '@typegoose/typegoose'
+import { ReturnModelType, getModelForClass, prop } from '@typegoose/typegoose'
 
 export class Message {
   @prop({ required: true })
-  text!: string
-  @prop({ required: true })
   user!: string
+
+  @prop({ required: true })
+  text!: string
+
+  public static findByUser(
+    this: ReturnModelType<typeof Message>,
+    user: string
+  ) {
+    return this.find({ user }).exec()
+  }
 }
 
 const MessageModel = getModelForClass(Message)
 
 export async function createMessage(user: string, text: string) {
-  return await new MessageModel({ user, text }).save
+  return await new MessageModel({ user, text }).save()
+}
+
+export async function getMessages(user: string) {
+  return await MessageModel.findByUser(user)
 }
