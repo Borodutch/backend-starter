2d037d826cc76376fc80791599fb639c8978810c
Jack <ipchchv@gmail.com>
2022-02-05 21:28:28 +0300

    Add tests for login and posting a message

---
+++ src/__tests__/message.ts
@@ -0,0 +1,48 @@
+import * as request from 'supertest'
+import * as shutdown from 'http-graceful-shutdown'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import { Mongoose } from 'mongoose'
+import { Server } from 'http'
+import runApp from '@/helpers/runApp'
+import runMongo from '@/helpers/mongo'
+
+describe('CRUD messages', () => {
+  let server: Server
+  let mongoServer: MongoMemoryServer
+  let mongoose: Mongoose
+  let token: string
+
+  beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create()
+    mongoose = await runMongo(await mongoServer.getUri())
+    server = await runApp()
+  })
+
+  afterAll(async () => {
+    await shutdown(server)
+    await mongoose.connection.db.dropDatabase()
+    await mongoServer.stop()
+    return new Promise<void>((resolve, reject) => {
+      server.close((err) => {
+        err ? reject(err) : resolve()
+      })
+    })
+  })
+
+  it('Log in', async () => {
+    const user = await request(server)
+      .post('/login/email')
+      .send({ email: 'johngalt@gmail.com', name: 'John' })
+    token = user.body.token
+    expect(user.statusCode).toBe(200)
+  })
+
+  it('Post message', async () => {
+    const response = await request(server)
+      .post('/message/')
+      .set('token', token)
+      .send({ text: 'Test text' })
+    expect(response.statusCode).toBe(200)
+    console.log(response.body)
+  })
+})
