4c2f3ecaf82f7f5aba0ca05cd61daa31d23c8d78
TurSer31 Сафаров <super.timati04@gmail.com>
2022-03-30 13:43:01 +0300

    fixes

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Ctx, Flow, Post } from 'amala'
+import { Body, Controller, Ctx, Post } from 'amala'
 import { Context } from 'koa'
 import { findOrCreateUser } from '@/models/User'
 import { forbidden } from '@hapi/boom'
@@ -7,7 +7,6 @@ import EmailLogin from '@/validators/EmailLogin'
 import FacebookLogin from '@/validators/FacebookLogin'
 import GoogleLogin from '@/validators/GoogleLogin'
 import TelegramLogin from '@/validators/TelegramLogin'
-import currentUser from '@/middleware/login'
 import getFBUser from '@/helpers/getFBUser'
 import getGoogleUser from '@/helpers/getGoogleUser'
 
@@ -51,7 +50,6 @@ export default class LoginController {
   }
 
   @Post('/email')
-  @Flow(currentUser)
   async emaiLogin(
     @Body({ required: true }) body: EmailLogin,
     @Ctx() ctx: Context
