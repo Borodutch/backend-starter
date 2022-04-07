621cac6d57fbf9b722dff5f10ef9b180b9e23e3e
Kurero <jkurero@gmail.com>
2022-04-08 01:45:13 +0300

    Changed error handling

--- src/helpers/errorHandler.ts
+++ src/helpers/errorHandler.ts
@@ -1,14 +1,8 @@
 import { Context, Next } from 'koa'
-import { boomify, methodNotAllowed, notFound } from '@hapi/boom'
+import { methodNotAllowed, notFound } from '@hapi/boom'
 
 export default async function (ctx: Context, next: Next) {
-  try {
-    await next()
-  } catch (error) {
-    if (error instanceof Error) {
-      throw boomify(error)
-    }
-  }
+  await next()
   const response = await ctx.body
   if (response === undefined) {
     return ctx.throw(methodNotAllowed())
