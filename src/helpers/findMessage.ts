4793ea31f11d462552eb15dfd983482f7c2de8d7
Kurero <jkurero@gmail.com>
2022-04-08 17:02:57 +0300

    cleaned up a little

--- src/helpers/findMessage.ts
+++ src/helpers/findMessage.ts
@@ -1,11 +1,10 @@
-import { CurrentUser } from 'amala'
 import { notFound } from '@hapi/boom'
 import MessageModel from '@/models/MessageModel'
 
 export default async function (messageId: string, currentUserId: string) {
   const message = await MessageModel.findById(messageId)
   if (currentUserId.toString() !== message?.author?.toString()) {
-    throw notFound(`there's no message with such id and author`)
+    throw notFound()
   }
   return message
 }
