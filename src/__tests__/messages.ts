cd084d1c978f608a2763205009ae08a64a95358c
Past21 <super.timati04@gmail.com>
2022-05-19 20:39:03 +0300

    tests with getting correct errors

--- src/__tests__/messages.ts
+++ src/__tests__/messages.ts
@@ -49,16 +49,28 @@ describe('CRUD endpoint', () => {
     expect(response.body.email).toBe(emailAccountMock.email)
   })
 
-  console.log(emailAccountMock.token)
-
   it('should return posted message for /messages request', async () => {
     const response = await request(server)
       .post('/messages')
       .set('token', emailAccountMock.token)
       .send(messageMock)
     messageMock.id = response.body._id
-    expect(response.statusCode).toBe(200)
     expect(response.body.text).toBe(messageMock.text)
+  })
+
+  it('should return correct error for incorrect post for /messages request', async () => {
+    const response = await request(server)
+      .post('/messages')
+      .set('token', '1337')
+      .send(messageMock)
+    expect(response.statusCode).toBe(403)
+  })
+  it('should return correct error for incorrect post for /messages request', async () => {
+    const response = await request(server)
+      .post('/messages')
+      .set('token', emailAccountMock.token)
+      .send({ text: true })
+    expect(response.statusCode).toBe(422)
   })
 
   it('should return one message for /messages request', async () => {
@@ -68,11 +80,29 @@ describe('CRUD endpoint', () => {
     expect(response.body.text).toBe(messageMock.text)
   })
 
+  it('should return correct error for getting one message for /messages request', async () => {
+    const response = await request(server)
+      .get(`/messages/1337`)
+      .set('token', emailAccountMock.token)
+    expect(response.statusCode).toBe(400)
+  })
+  it('should return correct error for getting one message for /messages request', async () => {
+    const response = await request(server)
+      .get(`/messages/${messageMock.id}`)
+      .set('token', '1337')
+    expect(response.statusCode).toBe(403)
+  })
+
   it('should return all messages of author for /messages request', async () => {
     const response = await request(server)
       .get('/messages')
       .set('token', emailAccountMock.token)
     expect(response.body[0].text).toBe(messageMock.text)
+  })
+
+  it('should return correct error for getting all messages of author for /messages request', async () => {
+    const response = await request(server).get('/messages').set('token', '1337')
+    expect(response.statusCode).toBe(403)
   })
 
   it('should return updated message for /messages request', async () => {
@@ -83,10 +113,45 @@ describe('CRUD endpoint', () => {
     expect(response.body.text).not.toBe(messageMock.text)
   })
 
+  it('should return correct error for updating message for /messages request', async () => {
+    const response = await request(server)
+      .patch(`/messages/1337`)
+      .set('token', emailAccountMock.token)
+      .send({ text: 'Aboba' })
+    expect(response.statusCode).toBe(400)
+  })
+  it('should return correct error for updating message for /messages request', async () => {
+    const response = await request(server)
+      .patch(`/messages/${messageMock.id}`)
+      .set('token', '1337')
+      .send({ text: 'Aboba' })
+    expect(response.statusCode).toBe(403)
+  })
+  it('should return correct error for updating message for /messages request', async () => {
+    const response = await request(server)
+      .patch(`/messages/${messageMock.id}`)
+      .set('token', emailAccountMock.token)
+      .send({ text: true })
+    expect(response.statusCode).toBe(422)
+  })
+
   it('should delete one message for /messages request', async () => {
     const response = await request(server)
       .delete(`/messages/${messageMock.id}`)
       .set('token', emailAccountMock.token)
     expect(response.statusCode).toBe(200)
+  })
+
+  it('should return correct error for deleting one message for /messages request', async () => {
+    const response = await request(server)
+      .delete(`/messages/1337`)
+      .set('token', emailAccountMock.token)
+    expect(response.statusCode).toBe(400)
+  })
+  it('should return correct error for deleting one message for /messages request', async () => {
+    const response = await request(server)
+      .delete(`/messages/${messageMock.id}`)
+      .set('token', '1337')
+    expect(response.statusCode).toBe(403)
   })
 })
