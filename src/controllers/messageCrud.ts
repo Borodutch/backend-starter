b038936a93e4e16b754ae756fb699833bae12e2c
EdisonsLamp <nichegohair@gmail.com>
2022-06-01 17:41:06 +0300

    added async/await

--- src/controllers/messageCrud.ts
+++ src/controllers/messageCrud.ts
@@ -2,29 +2,48 @@ import { Message } from "@/models/messageInterface"
 import { MessageModel } from "@/models/MessageModel"
 
 export const getAllMessages = async () => {
-    const allMessages = MessageModel.find()
-        .sort({ createdAt: -1 })
-        .then()
-        .catch(err => console.log(err.message));
-    return allMessages;
+    try {
+        const allMessages = await MessageModel.find().sort({ createdAt: -1 });
+        return allMessages;
+    } catch (err: any) {
+        console.log(err.message);
+    }
 }
 
 export const createMessage = async (message: Message) => {
     const request: Message = message;
     const newMessage = new MessageModel(request);
-    newMessage.save()
-        .then((result: any) => console.log(result))
-        .catch((err: any) => console.log(err.message))
+    try {
+        await newMessage.save();
+        return { isCreated: true }
+    }
+    catch (err: any) {
+        console.log(err.message);
+        return { isCreated: false }
+    }
 }
 
+
 export const updateMessage = async (message: Message) => {
-    MessageModel.findOneAndUpdate({ _id: message._id }, { title: message.title, body: message.body }, { new: true })
-        .then((result: any) => console.log(result))
-        .catch((err: any) => console.log(err.message))
+    try {
+        await MessageModel.findOneAndUpdate(
+            { _id: message._id },
+            { title: message.title, body: message.body },
+            { new: true }
+        );
+        return { isUpdated: true }
+    } catch (err: any) {
+        console.log(err.message);
+        return { isUpdated: false }
+    }
 }
 
 export const deleteMessage = async (id: string) => {
-    MessageModel.deleteOne({ _id: id })
-        .then((result: any) => console.log(result))
-        .catch((err: any) => console.log(err.message))
+    try {
+        await MessageModel.deleteOne({ _id: id });
+        return { isDeleted: true }
+    } catch (err: any) {
+        console.log(err.message);
+        return { isDeleted: false }
+    }
 }
--- src/controllers/messageRouterCrud.ts
+++ src/controllers/messageRouterCrud.ts
@@ -8,24 +8,40 @@ import * as crudService from './messageCrud'
 class MessageController {
     @Get('/all')
     async getMessageList(@Body() ctx: Context) {
-        return await crudService.getAllMessages()
+        try {
+            return await crudService.getAllMessages();
+        } catch (err: any) {
+            console.log(err.message);
+        }
     }
 
     @Post('/post')
     async createMessage(@Body({ required: true }) ctx: Message) {
-        await crudService.createMessage(ctx)
-        return { isCreated: true }
+        try {
+            const result = await crudService.createMessage(ctx);
+            return result;
+        } catch (err: any) {
+            console.log(err.message);
+        }
     }
 
     @Put('/update')
     async updateMessage(@Body() ctx: Message) {
-        await crudService.updateMessage(ctx)
-        return { isUpdated: true }
+        try {
+            const result = await crudService.updateMessage(ctx);
+            return result;
+        } catch (err: any) {
+            console.log(err.message);
+        }
     }
 
     @Put('/delete')
     async deleteMessage(@Body('_id') id: string) {
-        await crudService.deleteMessage(id)
-        return { isDeleted: true }
+        try {
+            const result = await crudService.deleteMessage(id);
+            return result;
+        } catch (err: any) {
+            console.log(err.message);
+        }
     }
 } 
