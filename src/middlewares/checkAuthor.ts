ca3df81fed79ef863990dd04443936a574928d4a
Sovetov Mikhail <sovotoffm@gmail.com>
2022-07-19 18:18:26 +0300

    Add error description to checkAuthor.ts

--- src/middlewares/checkAuthor.ts
+++ src/middlewares/checkAuthor.ts
@@ -6,15 +6,15 @@ export default async function checkAuthor(ctx: Context, next: Next) {
   const id = ctx.params.id
   const message = await MessageModel.findById(id)
   if (!message) {
-    return ctx.throw(notFound())
+    return ctx.throw(notFound('Message not found'))
   }
   if (!message.author) {
-    return ctx.throw(notFound())
+    return ctx.throw(notFound('Author not found'))
   }
   const userId = ctx.state.user.id
   const messageAuthor = message.author.toString()
   if (userId !== messageAuthor) {
-    return ctx.throw(notFound())
+    return ctx.throw(notFound('Author not found'))
   }
   ctx.state.message = message
   return next()
