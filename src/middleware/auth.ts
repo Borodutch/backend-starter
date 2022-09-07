f58c23d761cfa8e3cc30694b0ee7bf890563866f
SecretLodge <boxofmonster@mail.ru>
2022-09-07 21:14:15 +0500

    update

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,7 @@
 import { Context, Next } from 'koa'
 import { UserModel } from '@/models/User'
 
-export default async function auth(ctx: Context, next: Next) {
+export default async function (ctx: Context, next: Next) {
   const user = await UserModel.findOne({ token: ctx.headers.token })
   if (!user) throw new Error('User not found')
   ctx.state.user = user
