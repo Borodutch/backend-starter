ec9620de1c96b2daa8e372fa594818219a151ab4
Valery Sl <110306381+ValerySl@users.noreply.github.com>
2022-10-30 21:26:41 +0300

    Add files via upload

---
+++ src/models/Message.ts
@@ -0,0 +1,19 @@
+import { Ref, getModelForClass, modelOptions, prop } from '@typegoose/typegoose'
+import { User } from '@/models/User'
+
+@modelOptions({
+  schemaOptions: { timestamps: true },
+})
+export class Message {
+  @prop({
+    ref: () => User,
+    required: true,
+    index: true,
+  })
+  author!: Ref<User>
+
+  @prop({ required: true })
+  text!: string
+}
+
+export const MessageModel = getModelForClass(Message)
