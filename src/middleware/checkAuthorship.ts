77078a5a524b465a204a127322b01f5fe779d453
Maksim <116438158+Tvoncher@users.noreply.github.com>
2022-10-31 10:58:48 +0000

    small fixes

--- src/middleware/checkAuthorship.ts
+++ src/middleware/checkAuthorship.ts
@@ -1,5 +1,6 @@
 import { Context, Next } from 'koa'
 import { MessageModel } from '@/models/Message'
+import { notFound } from '@hapi/boom'
 
 export default async function checkAuthorship(ctx: Context, next: Next) {
   const id = ctx.params.id
@@ -7,10 +8,10 @@ export default async function checkAuthorship(ctx: Context, next: Next) {
   const message = await MessageModel.findById(id)
   ctx.state.message = message
   if (!message) {
-    return ctx.throw(404, 'not found')
+    return ctx.throw(notFound('not found'))
   }
   if (message.author != user.id) {
-    return ctx.throw(404, 'not found')
+    return ctx.throw(notFound('not found'))
   }
   return next()
 }
