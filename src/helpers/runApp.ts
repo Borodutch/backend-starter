f481b7728dcadfedef413bdd3fcd61fa1f076370
In-Red-We-Sky <112495830+In-Red-We-Sky@users.noreply.github.com>
2023-01-04 12:29:07 +0000

    СRUD test

--- src/helpers/runApp.ts
+++ src/helpers/runApp.ts
@@ -6,6 +6,52 @@ import { Server } from 'http'
 import { bootstrapControllers } from 'amala'
 import { resolve } from 'path'
 import env from '@/helpers/env'
+import mongoose from 'mongoose'
+
+require('./CRUDmodel')
+
+const Message_model = mongoose.model('message collection')
+
+// 1 - Create, 2 - Read, 3 - Update, 4 - Delete
+var w = 1
+
+switch (w) {
+  case 1:
+    // 01 Create
+    const message_m = new Message_model({ text_field: 'pretty good text' })
+    message_m
+      .save()
+      .then((m: any) => console.log(m))
+      .catch((e: any) => console.log(e))
+    break
+  case 2:
+    // 02 Read
+    Message_model.find({}).then((m: any) => {
+      console.log(m)
+    })
+    break
+  case 3:
+    // 03 Update
+    Message_model.updateMany(
+      { text_field: 'pretty good text' },
+      { text_field: 'pretty good text2' }
+    )
+      .then(() => {
+        console.log('updated')
+      })
+      .catch((e: any) => console.log(e))
+    break
+  case 4:
+    // 04 Delete
+    Message_model.find({ text_field: 'pretty good text2' })
+      .deleteMany()
+      .then(() => {
+        console.log('deleted')
+      })
+      .catch((e: any) => console.log(e))
+    break
+  default:
+}
 
 const app = new Koa()
 
